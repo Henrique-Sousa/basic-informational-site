279bc95d13e3175c39092837c6384e5aebce0a7e
Henrique Sousa <sh4dowonth3w4ll@gmail.com>
2020-01-28 17:20:18 -0300

    change + sign concatenation to path.join()

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(path.join(__dirname, '/index.html'));
 });
+
 app.get('/about', (req, res) => {
-	res.sendFile(__dirname + '/about.html');
+	res.sendFile(path.join(__dirname, '/about.html'));
 });
+
 app.get('/contact-me', (req, res) => {
-	res.sendFile(__dirname + '/contact-me.html');
+	res.sendFile(path.join(__dirname, '/contact-me.html'));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/404.html");
+    res.sendFile(path.join(__dirname, '/404.html'));
 });
 
 app.listen(8080);
